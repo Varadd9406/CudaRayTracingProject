f8f43464830117a2308e475e7538ec6d46127dfc
Varad Deouskar <varadd@gmail.com>
2021-01-16 04:11:06 +0530

    utiliy funcs

---
+++ utility.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <math.h>
+
+
+// Constants
+
+#define infinity INT32_MAX
+#define pi 3.1415926535897932385
+
+// Utility Functions
+
+
+__device__ __host__
+double degrees_to_radians(double degrees)
+{
+	double ans = (degrees*pi)/180.0;
+    return ans;
+}
+
+// Common Headers
+
